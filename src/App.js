b0ea4ebdab8f0452128e26672e26b7b723a7c2ac
Jonathan Cai <caimjonathan@gmail.com>
2020-10-02 16:43:54 -0700

    I get sad sometimes (testing deployment)

--- src/App.js
+++ src/App.js
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sometimes I get sad.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
