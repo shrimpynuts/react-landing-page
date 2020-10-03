d1e03906a54277811ef869d6b49f7e01bf2c668f
Jonathan Cai <caimjonathan@gmail.com>
2020-10-02 18:45:18 -0700

    add features section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import heroImage from '../assets/hero-img.png';
+import ft1 from '../assets/ft1.gif';
+import ft2 from '../assets/ft2.gif';
+import ft3 from '../assets/ft3.gif';
 
 export default function Home() {
     return (
         <div>
             <div className="hero">
                 <h1>Figure out what your audience wants.</h1>
-                <h2>We'll help you talk to your users.</h2>
+                <h2>We help you talk to users.</h2>
                 <a href="#form"><button>Join the waitlist</button></a>
                 <p>You're early!</p>
                 <img src={heroImage} width="100%" />
@@ -16,12 +19,37 @@ export default function Home() {
                 <p>Typically you want to do something to talk to your users.</p>
                 <p>But viewers can be from many different channels: SEO, subscribers, organic traffic.</p>
                 <p>Why don't we aggregate all of these different streams to give a single source of feedback for your content.</p>
+                <p>Here are all the features we plan to build:</p>
             </div>
-            <div className="form">
-                <section id="form">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfvCK0XfUG4Ps0ZHPbF9EVqXpXnbMjIS8h9S4iC9b_cEVihlQ/viewform?embedded=true" width="640" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-                </section>
-            </div>
+
+            <section className="features">
+                <div className="single-feature" id="left">
+                    <img src={ft1} />
+                    <div>
+                        <h3>Feature 1: Stripe integration</h3>
+                        <p>Stuff that is awesome.</p>
+                    </div>
+                </div>
+                <div className="single-feature" id="right">
+                    <div>
+                        <h3>Feature 2: Something great</h3>
+                        <p>Stuff that is awesome.</p>
+                    </div>
+                    <img src={ft2} />
+                </div>
+                <div className="single-feature" id="left">
+                    <img src={ft3} />
+                    <div>
+                        <h3>Feature 3: Awesome sauce</h3>
+                        <p>Stuff that is awesome.</p>
+                    </div>
+                </div>
+
+            </section>
+
+            <section id="form" className="form">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfvCK0XfUG4Ps0ZHPbF9EVqXpXnbMjIS8h9S4iC9b_cEVihlQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </section>
         </div>
     )
 }
