08f7442f02a88a3e73f77304e9672aaec39a09a6
Jonathan Cai <caimjonathan@gmail.com>
2020-10-05 23:46:20 -0700

    new promo expiration date

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   const [show, setShow] = useState(true);
 
-  let dealExpirationDate = new Date("10/20/2020"); 
+  let dealExpirationDate = new Date("12/20/2020"); 
   let today = new Date();
   let daysUntilDealEnds = Math.floor((dealExpirationDate.getTime() - today.getTime()) / (1000*60*60*24));
 
